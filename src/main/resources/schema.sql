9df90c13b71538f66eefb0db87edc1a0799a9309
Asrith123 <arasrith@gmail.com>
2019-12-17 00:47:44 +0530

    database added

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table book(id int, title varchar(20), author varchar(20), year varchar(20));
+insert into book values(101, 'Asrith', 'Asrith', '1998');
+insert into book values(102, 'Asrith', 'Asrith', '1997');
+insert into book values(103, 'Asrith', 'Asrith', '1997');
+insert into book values(104, 'Asrith', 'Asrith', '1999');
